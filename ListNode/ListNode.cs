0a40bad0410d03739299bc3e6c2cc93a860264ff
shaan arshaqu <shaanarshaqu123@gmail.com>
2024-06-27 09:40:02 +0530

    Add project files.

---
+++ ListNode/ListNode.cs
@@ -0,0 +1,81 @@
+﻿namespace ListNodes
+{
+    public class ListNode
+    {
+        int val;
+        ListNode next;
+        public ListNode(int val, ListNode next=null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+
+
+        public static ListNode head = null;
+        public static ListNode tail = null;
+
+        public static void AddToListNode(int val)
+        {
+            var newNode = new ListNode(val);
+            if (tail != null)
+            {
+                tail.next = newNode;
+                tail = tail.next;
+                return;
+            }
+            if (head == null)
+            {
+                head = newNode;
+                return;
+            }
+            ListNode current = head;
+            while (current != null) 
+            { 
+                if (current.next == null)
+                {
+                    current.next = newNode;
+                    return;
+                }
+                current= current.next;
+            }
+        }
+
+        public static void ViewList(ListNode current)
+        {
+/*            ListNode current = head;
+*/            Console.Write("[ ");
+            while (current != null)
+            {
+                if(current.next != null)
+                {
+                    Console.Write($"{current.val},");
+                }
+                else
+                {
+                    Console.Write($"{current.val} ");
+                }           
+                current = current.next;
+            }
+            Console.Write("]");
+        }
+
+        public async static Task<ListNode> RemoveDuplicate(ListNode refhead)
+        {
+            ListNode current = refhead;
+            ListNode prev = refhead;
+            while (current != null)
+            {
+                if (prev.val != current.val)
+                {
+                    prev.next = current;
+                    prev= prev.next;
+                }
+                current = current.next;
+            }
+            return refhead;
+        }
+
+        public static 
+
+    }
+}
---
+++ ListNode/Program.cs
@@ -0,0 +1,20 @@
+﻿using ListNodes;
+
+public class Program
+{
+    public async static Task Main(string[] args)
+    {
+        ListNode.AddToListNode(1);
+        ListNode.AddToListNode(1);
+        ListNode.AddToListNode(1);
+        ListNode.AddToListNode(1);
+        ListNode.AddToListNode(2);
+        ListNode.AddToListNode(2);
+        ListNode.AddToListNode(2);
+        ListNode.AddToListNode(2);
+        ListNode.AddToListNode(3);
+        ListNode.AddToListNode(5);
+
+        ListNode.ViewList(ListNode.head);
+    }
+}
